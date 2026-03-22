ba275973a3c339262ff4f4a6bd13101525334e12
abhay447 <abhay447@gmail.com>
2026-03-22 10:34:20 +0530

    added more details to shell prompt

--- src/tools/shellTools.ts
+++ src/tools/shellTools.ts
@@ -5,7 +5,10 @@ import { runShell } from "../utils/shellUtils";
 export class ShellExecute implements AgentTool {
   name: string = 'ShellExecute'
   toolDescription: string = `
-    This tools allows the agent to execute shell commands. Use it only when other tools fail. Arguments {"shell_command" : <shell_script>}
+    This tools allows the agent to execute shell commands. 
+    Use it only when other tools are not relevant for current goal. 
+    Try not to load a lot of data in memory , use piped/ chained shell execution if possible.
+    Arguments {"shell_command" : <shell_script>}
   `
   async execute(input: string): Promise<string> {
     let jsonInput = JSON.parse(input);
